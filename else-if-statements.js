3a01e0514005c0bcc33d62abb71b643d99701eb3
James Inglis <james.a.i.inglis@gmail.com>
2017-12-06 21:34:29 +1300

    added else if statements file

---
+++ else-if-statements.js
@@ -0,0 +1,16 @@
+const moonPhase = 'full';
+
+switch (moonPhase) {
+  case 'full':
+    console.log('Howwwwlll!!');
+    break;
+  case 'mostly full':
+    console.log('Arms and legs are getting hairier');
+    break;
+  case 'mostly new':
+    console.log('Back on two feet');
+    break;
+  default:
+    console.log('Invalid moon phase');
+    break;
+}
