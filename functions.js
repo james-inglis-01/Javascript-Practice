06fc10d0fc0db4cb7cd2c53768afba1d3a950823
James Inglis <james.a.i.inglis@gmail.com>
2017-12-09 18:31:31 +1300

    added functions review

--- functions.js
+++ functions.js
@@ -175,3 +175,19 @@ Finally, on the last line, we interpolated the function call within a console.lo
 
 We can use functions to section off small bits of logic or tasks, then use them when we need to. Writing functions can help take large and difficult problems and break them into small and manageable problems.
 */
+
+
+
+
+
+/*
+Review Functions
+This unit introduced you to functions.
+
+Functions are written to perform a task.
+Functions take data or variables, perform a set of tasks on them, and then return the result.
+We can define parameters when calling the function.
+When calling a function, we can pass in arguments, which will set the function's parameters.
+We can use return to return the result of a function which allows us to call functions anywhere, even inside other functions.
+Great work so far. Next up: Scope! Scope informs us where variables are accessible from within our programs.
+*/
