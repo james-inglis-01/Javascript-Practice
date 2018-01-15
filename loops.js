33fd512b59b792f87d1ace0f0a747aa1be0c82cb
James Inglis <james.a.i.inglis@gmail.com>
2018-01-15 23:08:29 +1300

    added data

--- loops.js
+++ loops.js
@@ -27,6 +27,33 @@ Inside the code block we can write any code we'd like to loop.
 
 //Profile lookup using a for loop nested if statements and dot and bracket notation
 
+var contacts = [
+  {
+      "firstName": "Akira",
+      "lastName": "Laine",
+      "number": "0543236543",
+      "likes": ["Pizza", "Coding", "Brownie Points"]
+  },
+  {
+      "firstName": "Harry",
+      "lastName": "Potter",
+      "number": "0994372684",
+      "likes": ["Hogwarts", "Magic", "Hagrid"]
+  },
+  {
+      "firstName": "Sherlock",
+      "lastName": "Holmes",
+      "number": "0487345643",
+      "likes": ["Intriguing Cases", "Violin"]
+  },
+  {
+      "firstName": "Kristian",
+      "lastName": "Vos",
+      "number": "unknown",
+      "likes": ["Javascript", "Gaming", "Foxes"]
+  }
+];
+
 function lookUpProfile(firstName, prop){
   
     for (var i = 0; i < contacts.length; i++) {
