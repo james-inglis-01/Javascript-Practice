7fa8e1d2c9f7a4f4b78bf26e93f104495aceec9f
James Inglis <james.a.i.inglis@gmail.com>
2018-03-30 08:54:07 +0100

    made a new file for algorithms

---
+++ algorithms.js
@@ -0,0 +1,11 @@
+//find a number in an array
+function findNumber(arr, k) {
+  for (var i = 0; i < arr.length; i++) {
+      if (arr[i] === k) {
+          return "YES";
+      }
+  }
+  return "NO";
+
+
+}
