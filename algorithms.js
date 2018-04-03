1c067f8c1e26938a5b2c01646aa45a70367bb023
James Inglis <james.a.i.inglis@gmail.com>
2018-04-03 12:09:07 +0100

    added an algorithm for factorializing a number

--- algorithms.js
+++ algorithms.js
@@ -1,4 +1,4 @@
-//find a number in an array if true then return YES
+//Find a number in an array, if true then return YES
 function findNumber(arr, k) {
   for (var i = 0; i < arr.length; i++) {
       if (arr[i] === k) {
@@ -9,3 +9,23 @@ function findNumber(arr, k) {
 
 
 }
+
+//Factorializing numbers
+function factorialize(num) {
+  var count = num;
+  //Store a count variable for for loop here
+  if(count === 0) {
+    return 1;
+    //Statement for the unique '0' scenario
+  } else {
+  
+  for(var i = 1; i < count; i++) {
+    num = num * i;
+    //Loop to factorialize starting at 1 and moving up to the end
+  }
+  
+  return num;
+}
+}
+
+factorialize(5);
